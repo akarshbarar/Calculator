b0b7d0f9a44ab220c18f0edbc1fca77491c3bc5f
Akarsh Barar <barar.akarsh8@gmail.com>
2021-10-18 22:27:06 +0530

    getx

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'controller/controller.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,7 +11,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var inputText = 0;
+  final Controller c = Get.put(Controller());
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,12 +24,18 @@ class _HomeState extends State<Home> {
               children: [
                 Expanded(
                     flex: 3,
-                    child: Text(
-                      inputText.toString(),
-                      style: TextStyle(color: Colors.white, fontSize: 100),
-                      textAlign: TextAlign.right,
-                      textScaleFactor: 1.5,
-                    )),
+                    child: Obx(
+                      () => Text(
+                        c.value.toString(),
+                        style: TextStyle(color: Colors.white, fontSize: 100),
+                        textAlign: TextAlign.right,
+                        textScaleFactor: 1.5,
+                      ),
+                    )
+                    //Text(
+
+                    //)
+                    ),
                 Expanded(
                     flex: 7,
                     child: Column(
@@ -34,64 +44,121 @@ class _HomeState extends State<Home> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            customButton("AC", Colors.grey[300], Colors.black,
-                                inputText),
-                            customButton("+/-", Colors.grey[300], Colors.black,
-                                inputText),
                             customButton(
-                                "%", Colors.grey[300], Colors.black, inputText),
-                            customButton("/", Colors.orange[300], Colors.white,
-                                inputText),
+                              "AC",
+                              Colors.grey[300],
+                              Colors.black,
+                            ),
+                            customButton(
+                              "+/-",
+                              Colors.grey[300],
+                              Colors.black,
+                            ),
+                            customButton(
+                              "%",
+                              Colors.grey[300],
+                              Colors.black,
+                            ),
+                            customButton(
+                              "/",
+                              Colors.orange[300],
+                              Colors.white,
+                            ),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             customButton(
-                                "7", Colors.grey[900], Colors.white, inputText),
+                              "7",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "8", Colors.grey[900], Colors.white, inputText),
+                              "8",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "9", Colors.grey[900], Colors.white, inputText),
-                            customButton("X", Colors.orange[300], Colors.white,
-                                inputText),
+                              "9",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
+                            customButton(
+                              "X",
+                              Colors.orange[300],
+                              Colors.white,
+                            ),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             customButton(
-                                "4", Colors.grey[900], Colors.white, inputText),
+                              "4",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "5", Colors.grey[900], Colors.white, inputText),
+                              "5",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "6", Colors.grey[900], Colors.white, inputText),
-                            customButton("-", Colors.orange[300], Colors.white,
-                                inputText),
+                              "6",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
+                            customButton(
+                              "-",
+                              Colors.orange[300],
+                              Colors.white,
+                            ),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             customButton(
-                                "1", Colors.grey[900], Colors.white, inputText),
+                              "1",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "2", Colors.grey[900], Colors.white, inputText),
+                              "2",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                "3", Colors.grey[900], Colors.white, inputText),
-                            customButton("+", Colors.orange[300], Colors.white,
-                                inputText),
+                              "3",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
+                            customButton(
+                              "+",
+                              Colors.orange[300],
+                              Colors.white,
+                            ),
                           ],
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             customButton(
-                                "0", Colors.grey[900], Colors.white, inputText),
+                              "0",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
                             customButton(
-                                ",", Colors.grey[900], Colors.white, inputText),
-                            customButton("=", Colors.orange[300], Colors.white,
-                                inputText),
+                              ",",
+                              Colors.grey[900],
+                              Colors.white,
+                            ),
+                            customButton(
+                              "=",
+                              Colors.orange[300],
+                              Colors.white,
+                            ),
                           ],
                         ),
                       ],
@@ -100,13 +167,11 @@ class _HomeState extends State<Home> {
             )));
   }
 
-  Widget customButton(text, backgroundColor, textColor, inputText) {
+  Widget customButton(text, backgroundColor, textColor) {
     return RawMaterialButton(
       onPressed: () {
         print(text);
-        setState(() {
-          inputText = text;
-        });
+        c.value.value = text;
       },
       elevation: 0,
       fillColor: backgroundColor,
